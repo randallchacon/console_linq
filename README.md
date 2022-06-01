351f189899aaf4761d4fe50a91f6a21d062269d8
randallchacon <info@randallchacon.com>
2022-05-31 19:15:31 -0600

    Basic linq code

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-#C# LINQ project
+#LINQ project
+#Language-Integrated Query
 ###About this project
 - The objective of this project is to use lists of elements with filters, using ordering, scalars. quantifiers, pagination, grouping, related collections, sets, miscellaneous, and data base with entity framework
--- console_linq/Program.cs
+++ console_linq/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+int[] numbers = { 1, 2, 3, 4, 5 };
+int[] numbersRange = Enumerable.Range(1, 20).ToArray();
+
+//Methods Syntax or Lambda Sintax
+var pairNumbers = numbers.Where(n => n % 2 == 0).ToList();
+
+//Queries syntax
+var pairNumbers2 = from n in numbers
+                   where n % 2 == 0
+                   select n;
+
+//Range
+var pairRangeNumbers = numbersRange.Where(n => n % 2 == 0).ToList();
+
 Console.WriteLine("Hello, World!");
