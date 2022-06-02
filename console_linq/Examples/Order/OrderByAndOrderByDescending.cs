3730c62966a78563e3bdbe1ba90809db333e6c2c
randallchacon <info@randallchacon.com>
2022-06-01 22:16:44 -0600

    c13. OrderBy and OrderByDescending

--- console_linq/Examples/Order/OrderByAndOrderByDescending.cs
+++ console_linq/Examples/Order/OrderByAndOrderByDescending.cs
@@ -12,6 +12,51 @@ namespace console_linq.Examples.Order
         public static void main()
         {
             var people = new People().peopleList;
+
+            var peopleOrderByAge = people.OrderBy(p => p.Age);
+
+            foreach(var person in peopleOrderByAge)
+            {
+                Console.WriteLine($"{person.Name} is {person.Age} years old.");
+            }
+
+            Console.WriteLine("----------------\n");
+
+            var peopleOrderByAgeDescending = people.OrderByDescending(p => p.Age);
+
+            foreach (var person in peopleOrderByAgeDescending)
+            {
+                Console.WriteLine($"{person.Name} is {person.Age} years old.");
+            }
+
+            int[] numbers = { 5, 10, 80, 12, 1, 9, 81 };
+
+            Console.WriteLine("----------------\n");
+
+            foreach (var n in numbers.OrderBy(n => n))
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine("----------------\n");
+            //Queries Syntax
+            var numbers2 = from n in numbers
+                           orderby n
+                           select n;
+
+            var peopleOrderByAgeDesc = from p in people
+                                       orderby p.Age descending
+                                       select p;
+
+            foreach(var n in numbers2){
+                Console.Write($"{n} ");
+            }
+
+            Console.WriteLine("\n----------------\n");
+            foreach (var person in peopleOrderByAgeDesc)
+            {
+                Console.WriteLine($"{person.Name} is {person.Age} years old.");
+            }
         }
     }
 }
--- console_linq/Program.cs
+++ console_linq/Program.cs
@@ -1,8 +1,11 @@
 ﻿/*Developed by Randall Chacón*/
 using console_linq.Objects.Menu;
 using console_linq.Examples;
+using console_linq.Examples.Order;
 
-Menu.show();
+//Menu.show();
+
+OrderByAndOrderByDescending.main();
 
 
 
